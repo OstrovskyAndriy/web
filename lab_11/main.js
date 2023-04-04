6f60023fba998b28e00751f44b3a8e37e579efda
Островський Андрій <106969285+OstrovskyAndriy@users.noreply.github.com>
2023-04-04 16:25:35 +0300

    Add files via upload

---
+++ lab_11/main.js
@@ -0,0 +1,41 @@
+console.log(alert("Островський Андрій"));
+
+let a=4;
+let b=5;
+console.log("a =",a);
+console.log("b =",b);
+
+a=b;
+console.log("a =",a);
+console.log("b =",b);
+
+let str=String("str");
+let number = Number(2);
+let boolean=Boolean(1);
+let undef=undefined;
+var nul=null;
+
+let isAdult=confirm("Вам є 18 років?");
+console.log("isAdult = ",isAdult);
+
+let name = "Андрій";
+let lastName = "Островський";
+let birthday = 2004;
+let family = false;
+console.log(typeof (name), typeof(lastName),typeof(birthday),typeof(family));
+
+let login = prompt("Enter your login");
+let email = prompt("Enter your email");
+let password = prompt("Enter your password");
+
+console.log("Dear", login, "your email is ", email, "your password: ", password);
+
+let result = 60 * 60;
+console.log("Кількість секунд в годинах", result);
+
+result = 60 * 60 * 24;
+console.log("Кількість секунд в добі", result);
+
+result = 3600 * 24 * 31;
+console.log("Кількість секунд в місяці", result);
+
